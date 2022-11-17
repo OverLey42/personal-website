6ee0abcc2572d7901ad7ef8eb0fe18f1550c5de8
OverLey24 <leyover512@gmail.com>
2022-11-17 02:14:13 +0100

    Fix for deno deploy

--- .svelte-kit/deno/server/nodes/1.js
+++ .svelte-kit/deno/server/nodes/1.js
@@ -2,6 +2,6 @@
 
 export const index = 1;
 export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/components/error.svelte-c220b6f3.js';
-export const imports = ["_app/immutable/components/error.svelte-c220b6f3.js","_app/immutable/chunks/index-beaab6d2.js","_app/immutable/chunks/singletons-d924a061.js"];
+export const file = '_app/immutable/components/error.svelte-26ec5d40.js';
+export const imports = ["_app/immutable/components/error.svelte-26ec5d40.js","_app/immutable/chunks/index-beaab6d2.js","_app/immutable/chunks/singletons-f255f348.js"];
 export const stylesheets = [];
