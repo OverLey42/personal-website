e5321ac72e091d5635b10979a1a43cba377ef070
OverLey24 <leyover512@gmail.com>
2022-11-30 20:01:26 +0100

    responsive footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,7 +19,7 @@
 			// (Computer)
 			anime({
 				targets: '.header',
-				fontSize: '9vw',
+				fontSize: '10vw',
 				easing: 'easeInOutQuint',
 				opacity: 100,
 				duration: 1500,
@@ -28,7 +28,7 @@
 			// (mobile)
 			anime({
 				targets: '.header',
-				fontSize: '24vw',
+				fontSize: '22vw',
 				easing: 'easeInOutQuint',
 				opacity: 100,
 				duration: 1500,
@@ -41,7 +41,7 @@
 				// (Computer)
 				anime({
 					targets: '.header',
-					fontSize: '9vw',
+					fontSize: '10vw',
 					easing: 'easeInOutQuint',
 					opacity: 100,
 					duration: 1500,
@@ -60,64 +60,67 @@
     });
 </script>
 
-<Lines></Lines>
+<main class="content min-h-[calc(100vh_-_40px)]">
 
-<h1 class="header font-extrabold text-transparent xl:text-[2vw] text-[8vw] bg-clip-text bg-gradient-to-r text-center mt-32 bg-[#141414] to-[#141414] via-purple-500 from-[#e20f8a] bg-size-200 bg-pos-0">OverLey</h1>
+	<Lines></Lines>
 
-<div class="social flex justify-center">
-	<div class="social-child flex flex-wrap outline outline-offset-8 rounded-xl text-center p-10 m-12">
-		<div class="flex-grow">
-			<h1><img class="rounded-full h-6 w-6 inline-block" src="https://imgs.search.brave.com/ij3t5KLpcnSaGFABUAAdPh9IARp5fsbQSBZBRQC7UWE/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9sb2dv/cy1kb3dubG9hZC5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MTYvMDkvR2l0SHVi/X2xvZ28ucG5n" alt=""> GitHub</h1>
-			<div class="github hover:-translate-y-1 hover:scale-110 transition-all duration-500 bg-[#141414] hover:bg-gradient-to-l to-[#141414] via-purple-500 hover:duration-1000 from-[#141414] bg-size-200 bg-pos-0 hover:bg-pos-100 outline-offset-4 p-7 justify-center rounded-xl shadow-lg flex items-center space-x-4 m-4 min-w-[200px] w-auto">
-				<div class="shrink-0">
-				  <img class="rounded-full h-12 w-12" src="https://avatars.githubusercontent.com/u/73314792?v=4" alt="ChitChat Logo">
-				</div>
-				<div>
-				  <h1>OverLey</h1>
-				  <p>overley24</p>
+	<h1 class="header font-mono font-extrabold text-transparent xl:text-[2vw] text-[8vw] bg-clip-text bg-gradient-to-r text-center mt-32 bg-[#141414] to-[#141414] via-purple-500 from-[#e20f8a] bg-size-200 bg-pos-0">OverLey</h1>
+
+	<div class="social flex justify-center">
+		<div class="social-child flex flex-wrap outline outline-offset-8 rounded-xl text-center p-10 m-12">
+			<div class="flex-grow">
+				<h1><img class="rounded-full h-6 w-6 inline-block" src="https://imgs.search.brave.com/ij3t5KLpcnSaGFABUAAdPh9IARp5fsbQSBZBRQC7UWE/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9sb2dv/cy1kb3dubG9hZC5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MTYvMDkvR2l0SHVi/X2xvZ28ucG5n" alt=""> GitHub</h1>
+				<div class="github hover:-translate-y-1 hover:scale-110 transition-all duration-500 bg-[#141414] hover:bg-gradient-to-l to-[#141414] via-purple-500 hover:duration-1000 from-[#141414] bg-size-200 bg-pos-0 hover:bg-pos-100 outline-offset-4 p-7 justify-center rounded-xl shadow-lg flex items-center space-x-4 m-4 min-w-[200px] w-auto">
+					<div class="shrink-0">
+					  <img class="rounded-full h-12 w-12" src="https://avatars.githubusercontent.com/u/73314792?v=4" alt="ChitChat Logo">
+					</div>
+					<div>
+					  <h1>OverLey</h1>
+					  <p>overley24</p>
+					</div>
 				</div>
 			</div>
-		</div>
-		
-		<div class="flex-grow">
-			<h1><img class="rounded-full h-6 w-6 inline-block" src="https://imgs.search.brave.com/ij3t5KLpcnSaGFABUAAdPh9IARp5fsbQSBZBRQC7UWE/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9sb2dv/cy1kb3dubG9hZC5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MTYvMDkvR2l0SHVi/X2xvZ28ucG5n" alt=""> GitHub</h1>
-			<div class="github hover:-translate-y-1 hover:scale-110 transition-all duration-500 bg-[#141414] hover:bg-gradient-to-l to-[#141414] via-purple-500 hover:duration-1000 from-[#141414] bg-size-200 bg-pos-0 hover:bg-pos-100 outline-offset-4 p-7 justify-center rounded-xl shadow-lg flex items-center space-x-4 m-4 min-w-[200px] w-auto">
-				<div class="shrink-0">
-				  <img class="rounded-full h-12 w-12" src="https://avatars.githubusercontent.com/u/73314792?v=4" alt="ChitChat Logo">
-				</div>
-				<div>
-				  <h1>OverLey</h1>
-				  <p>overley24</p>
-				</div>
-			</div>
-		</div>
-
-		<div class="flex-grow">
-			<h1><img class="rounded-full h-6 w-6 inline-block" src="https://imgs.search.brave.com/ij3t5KLpcnSaGFABUAAdPh9IARp5fsbQSBZBRQC7UWE/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9sb2dv/cy1kb3dubG9hZC5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MTYvMDkvR2l0SHVi/X2xvZ28ucG5n" alt=""> GitHub</h1>
-			<div class="github hover:-translate-y-1 hover:scale-110 transition-all duration-500 bg-[#141414] hover:bg-gradient-to-l to-[#141414] via-purple-500 hover:duration-1000 from-[#141414] bg-size-200 bg-pos-0 hover:bg-pos-100 outline-offset-4 p-7 justify-center rounded-xl shadow-lg flex items-center space-x-4 m-4 min-w-[200px] w-auto">
-				<div class="shrink-0">
-				  <img class="rounded-full h-12 w-12" src="https://avatars.githubusercontent.com/u/73314792?v=4" alt="ChitChat Logo">
-				</div>
-				<div>
-				  <h1>OverLey</h1>
-				  <p>overley24</p>
+			
+			<div class="flex-grow">
+				<h1><img class="rounded-full h-6 w-6 inline-block" src="https://imgs.search.brave.com/ij3t5KLpcnSaGFABUAAdPh9IARp5fsbQSBZBRQC7UWE/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9sb2dv/cy1kb3dubG9hZC5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MTYvMDkvR2l0SHVi/X2xvZ28ucG5n" alt=""> GitHub</h1>
+				<div class="github hover:-translate-y-1 hover:scale-110 transition-all duration-500 bg-[#141414] hover:bg-gradient-to-l to-[#141414] via-purple-500 hover:duration-1000 from-[#141414] bg-size-200 bg-pos-0 hover:bg-pos-100 outline-offset-4 p-7 justify-center rounded-xl shadow-lg flex items-center space-x-4 m-4 min-w-[200px] w-auto">
+					<div class="shrink-0">
+					  <img class="rounded-full h-12 w-12" src="https://avatars.githubusercontent.com/u/73314792?v=4" alt="ChitChat Logo">
+					</div>
+					<div>
+					  <h1>OverLey</h1>
+					  <p>overley24</p>
+					</div>
 				</div>
 			</div>
-		</div>
-
-		<div class="flex-grow">
-			<h1><img class="rounded-full h-6 w-6 inline-block" src="https://imgs.search.brave.com/ij3t5KLpcnSaGFABUAAdPh9IARp5fsbQSBZBRQC7UWE/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9sb2dv/cy1kb3dubG9hZC5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MTYvMDkvR2l0SHVi/X2xvZ28ucG5n" alt=""> GitHub</h1>
-			<div class="github hover:-translate-y-1 hover:scale-110 transition-all duration-500 bg-[#141414] hover:bg-gradient-to-l to-[#141414] via-purple-500 hover:duration-1000 from-[#141414] bg-size-200 bg-pos-0 hover:bg-pos-100 outline-offset-4 p-7 justify-center rounded-xl shadow-lg flex items-center space-x-4 m-4 min-w-[200px] w-auto">
-				<div class="shrink-0">
-				  <img class="rounded-full h-12 w-12" src="https://avatars.githubusercontent.com/u/73314792?v=4" alt="ChitChat Logo">
+	
+			<div class="flex-grow">
+				<h1><img class="rounded-full h-6 w-6 inline-block" src="https://imgs.search.brave.com/ij3t5KLpcnSaGFABUAAdPh9IARp5fsbQSBZBRQC7UWE/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9sb2dv/cy1kb3dubG9hZC5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MTYvMDkvR2l0SHVi/X2xvZ28ucG5n" alt=""> GitHub</h1>
+				<div class="github hover:-translate-y-1 hover:scale-110 transition-all duration-500 bg-[#141414] hover:bg-gradient-to-l to-[#141414] via-purple-500 hover:duration-1000 from-[#141414] bg-size-200 bg-pos-0 hover:bg-pos-100 outline-offset-4 p-7 justify-center rounded-xl shadow-lg flex items-center space-x-4 m-4 min-w-[200px] w-auto">
+					<div class="shrink-0">
+					  <img class="rounded-full h-12 w-12" src="https://avatars.githubusercontent.com/u/73314792?v=4" alt="ChitChat Logo">
+					</div>
+					<div>
+					  <h1>OverLey</h1>
+					  <p>overley24</p>
+					</div>
 				</div>
-				<div>
-				  <h1>OverLey</h1>
-				  <p>overley24</p>
+			</div>
+	
+			<div class="flex-grow">
+				<h1><img class="rounded-full h-6 w-6 inline-block" src="https://imgs.search.brave.com/ij3t5KLpcnSaGFABUAAdPh9IARp5fsbQSBZBRQC7UWE/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9sb2dv/cy1kb3dubG9hZC5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MTYvMDkvR2l0SHVi/X2xvZ28ucG5n" alt=""> GitHub</h1>
+				<div class="github hover:-translate-y-1 hover:scale-110 transition-all duration-500 bg-[#141414] hover:bg-gradient-to-l to-[#141414] via-purple-500 hover:duration-1000 from-[#141414] bg-size-200 bg-pos-0 hover:bg-pos-100 outline-offset-4 p-7 justify-center rounded-xl shadow-lg flex items-center space-x-4 m-4 min-w-[200px] w-auto">
+					<div class="shrink-0">
+					  <img class="rounded-full h-12 w-12" src="https://avatars.githubusercontent.com/u/73314792?v=4" alt="ChitChat Logo">
+					</div>
+					<div>
+					  <h1>OverLey</h1>
+					  <p>overley24</p>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
+</main>
 
 <Footer></Footer>
