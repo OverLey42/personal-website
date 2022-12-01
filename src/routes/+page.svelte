b1d984f417b0d1b96fac35acd7c5343f0555eeb0
OverLey24 <leyover512@gmail.com>
2022-12-01 18:06:35 +0100

    more footer responsive changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -66,11 +66,12 @@
 
 	<h1 class="header font-mono font-extrabold text-transparent xl:text-[2vw] text-[8vw] bg-clip-text bg-gradient-to-r text-center mt-32 bg-[#141414] to-[#141414] via-purple-500 from-[#e20f8a] bg-size-200 bg-pos-0">OverLey</h1>
 
-	<div class="social flex justify-center">
-		<div class="social-child flex flex-wrap outline outline-offset-8 rounded-xl text-center p-10 m-12">
+	<div class="social flex flex-col justify-center text-center">
+		<h1>Socials</h1>
+		<div class="social-child flex flex-wrap outline outline-offset-8 rounded-xl text-center p-10 m-12 ml-88 mr-[12vw] ml-[12vw]">
 			<div class="flex-grow">
 				<h1><img class="rounded-full h-6 w-6 inline-block" src="https://imgs.search.brave.com/ij3t5KLpcnSaGFABUAAdPh9IARp5fsbQSBZBRQC7UWE/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9sb2dv/cy1kb3dubG9hZC5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MTYvMDkvR2l0SHVi/X2xvZ28ucG5n" alt=""> GitHub</h1>
-				<div class="github hover:-translate-y-1 hover:scale-110 transition-all duration-500 bg-[#141414] hover:bg-gradient-to-l to-[#141414] via-purple-500 hover:duration-1000 from-[#141414] bg-size-200 bg-pos-0 hover:bg-pos-100 outline-offset-4 p-7 justify-center rounded-xl shadow-lg flex items-center space-x-4 m-4 min-w-[200px] w-auto">
+				<div class="github hover:-translate-y-1 hover:scale-110 transition-all duration-500 bg-[#141414] hover:bg-gradient-to-l to-[#141414] via-purple-500 hover:duration-1000 from-[#141414] bg-size-200 bg-pos-0 hover:bg-pos-100 outline-offset-4 p-7 justify-center rounded-xl shadow-lg flex items-center space-x-4 m-4 w-auto">
 					<div class="shrink-0">
 					  <img class="rounded-full h-12 w-12" src="https://avatars.githubusercontent.com/u/73314792?v=4" alt="ChitChat Logo">
 					</div>
@@ -83,7 +84,7 @@
 			
 			<div class="flex-grow">
 				<h1><img class="rounded-full h-6 w-6 inline-block" src="https://imgs.search.brave.com/ij3t5KLpcnSaGFABUAAdPh9IARp5fsbQSBZBRQC7UWE/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9sb2dv/cy1kb3dubG9hZC5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MTYvMDkvR2l0SHVi/X2xvZ28ucG5n" alt=""> GitHub</h1>
-				<div class="github hover:-translate-y-1 hover:scale-110 transition-all duration-500 bg-[#141414] hover:bg-gradient-to-l to-[#141414] via-purple-500 hover:duration-1000 from-[#141414] bg-size-200 bg-pos-0 hover:bg-pos-100 outline-offset-4 p-7 justify-center rounded-xl shadow-lg flex items-center space-x-4 m-4 min-w-[200px] w-auto">
+				<div class="github hover:-translate-y-1 hover:scale-110 transition-all duration-500 bg-[#141414] hover:bg-gradient-to-l to-[#141414] via-purple-500 hover:duration-1000 from-[#141414] bg-size-200 bg-pos-0 hover:bg-pos-100 outline-offset-4 p-7 justify-center rounded-xl shadow-lg flex items-center space-x-4 m-4 min-w-[100px] w-auto">
 					<div class="shrink-0">
 					  <img class="rounded-full h-12 w-12" src="https://avatars.githubusercontent.com/u/73314792?v=4" alt="ChitChat Logo">
 					</div>
@@ -96,7 +97,7 @@
 	
 			<div class="flex-grow">
 				<h1><img class="rounded-full h-6 w-6 inline-block" src="https://imgs.search.brave.com/ij3t5KLpcnSaGFABUAAdPh9IARp5fsbQSBZBRQC7UWE/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9sb2dv/cy1kb3dubG9hZC5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MTYvMDkvR2l0SHVi/X2xvZ28ucG5n" alt=""> GitHub</h1>
-				<div class="github hover:-translate-y-1 hover:scale-110 transition-all duration-500 bg-[#141414] hover:bg-gradient-to-l to-[#141414] via-purple-500 hover:duration-1000 from-[#141414] bg-size-200 bg-pos-0 hover:bg-pos-100 outline-offset-4 p-7 justify-center rounded-xl shadow-lg flex items-center space-x-4 m-4 min-w-[200px] w-auto">
+				<div class="github hover:-translate-y-1 hover:scale-110 transition-all duration-500 bg-[#141414] hover:bg-gradient-to-l to-[#141414] via-purple-500 hover:duration-1000 from-[#141414] bg-size-200 bg-pos-0 hover:bg-pos-100 outline-offset-4 p-7 justify-center rounded-xl shadow-lg flex items-center space-x-4 m-4 min-w-[100px] w-auto">
 					<div class="shrink-0">
 					  <img class="rounded-full h-12 w-12" src="https://avatars.githubusercontent.com/u/73314792?v=4" alt="ChitChat Logo">
 					</div>
@@ -109,7 +110,7 @@
 	
 			<div class="flex-grow">
 				<h1><img class="rounded-full h-6 w-6 inline-block" src="https://imgs.search.brave.com/ij3t5KLpcnSaGFABUAAdPh9IARp5fsbQSBZBRQC7UWE/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9sb2dv/cy1kb3dubG9hZC5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MTYvMDkvR2l0SHVi/X2xvZ28ucG5n" alt=""> GitHub</h1>
-				<div class="github hover:-translate-y-1 hover:scale-110 transition-all duration-500 bg-[#141414] hover:bg-gradient-to-l to-[#141414] via-purple-500 hover:duration-1000 from-[#141414] bg-size-200 bg-pos-0 hover:bg-pos-100 outline-offset-4 p-7 justify-center rounded-xl shadow-lg flex items-center space-x-4 m-4 min-w-[200px] w-auto">
+				<div class="github hover:-translate-y-1 hover:scale-110 transition-all duration-500 bg-[#141414] hover:bg-gradient-to-l to-[#141414] via-purple-500 hover:duration-1000 from-[#141414] bg-size-200 bg-pos-0 hover:bg-pos-100 outline-offset-4 p-7 justify-center rounded-xl shadow-lg flex items-center space-x-4 m-4 min-w-[100px] w-auto">
 					<div class="shrink-0">
 					  <img class="rounded-full h-12 w-12" src="https://avatars.githubusercontent.com/u/73314792?v=4" alt="ChitChat Logo">
 					</div>
--- src/routes/footer/+page.svelte
+++ src/routes/footer/+page.svelte
@@ -3,9 +3,9 @@
     import "../../app.css";
 </script>
 
-<div class="footer text-center">
+<div class="footer text-center h-[50px]">
     <div class="image flex justify-center text-center">
-        <img class="cat md:w-[100%] lg:w-[80%] 2xl:w-[55%]" src="cat-stars.png" alt="" title="image by rain <3">
+        <img class="cat md:w-[100%] lg:w-[40%] 2xl:w-[40%]" src="cat-stars.png" alt="" title="image by rain <3">
     </div>    
 </div>
 
