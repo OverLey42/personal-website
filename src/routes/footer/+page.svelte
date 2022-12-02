4fdcf36c06a2d51871bff354a7f26badc2ab4111
OverLey24 <leyover512@gmail.com>
2022-12-02 20:07:44 +0100

    shooting star improvement

--- src/routes/footer/+page.svelte
+++ src/routes/footer/+page.svelte
@@ -5,22 +5,22 @@
     import "../../app.css";
 
 
-        function test() {
-			console.log("test");
+        function shootingStar() {
             anime({
-				targets: '.stars',
+				targets: '.shooting-star',
 				marginLeft: '80%',
 				easing: 'easeInOutQuint',
-				opacity: [10, 0],
-				duration: 3500,
+				opacity: [5, 0],
+				duration: 2500,
 			});
 	}
   
 </script>
 
-<p class="stars opacity-1 ml-[5%]">. . . o</p>
+<p class="shooting-star opacity-0 ml-[-40%]">. . . o</p>
 <div class="footer text-center h-[50px]">
-    <div class="image flex justify-center text-center" on:click={test}>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div class="image flex justify-center text-center" on:click={shootingStar}>
         <img class="cat w-[75%] md:w-[80%] lg:w-[40%] 2xl:w-[40%]" src="cat-stars.png" alt="" title="image by rain <3">
     </div>    
 </div>
